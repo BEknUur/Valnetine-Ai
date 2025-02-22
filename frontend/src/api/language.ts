03ed1a026e4b316be83a6ff4cf8407db443b935c
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-02-23 01:17:03 +0500

    che tam 2

--- frontend/src/api/language.ts
+++ frontend/src/api/language.ts
@@ -1,11 +1,12 @@
+const API_URL = "https://valnetine-ai.onrender.com";  
+
 export async function fetchSupportedLanguages() {
     try {
-      const response = await fetch("http://localhost:8000/supported_languages");
+      const response = await fetch(`${API_URL}/supported_languages`);
       const data = await response.json();
       return data.languages;
     } catch (error) {
       console.error("Error fetching languages:", error);
       return [];
     }
-  }
-  
+}
